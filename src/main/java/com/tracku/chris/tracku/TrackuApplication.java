f62d898b2c778646e3145d65b88c4010c66ad921
ChrisEs146 <chris.es146@gmail.com>
2022-12-20 17:45:04 -0600

    First commit

---
+++ src/main/java/com/tracku/chris/tracku/TrackuApplication.java
@@ -0,0 +1,13 @@
+package com.tracku.chris.tracku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tracku/chris/tracku/TrackuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tracku.chris.tracku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
