37578fe5169da71b799bb3a4b2be532322f32fb9
ChrisEs146 <chris.es146@gmail.com>
2023-01-07 21:35:27 -0600

    Add: findbyName method to user repository

--- src/main/java/com/tracku/chris/tracku/Repositories/UserRepository.java
+++ src/main/java/com/tracku/chris/tracku/Repositories/UserRepository.java
@@ -1,8 +1,11 @@
 package com.tracku.chris.tracku.Repositories;
-import com.tracku.chris.tracku.Entities.User;
+import com.tracku.chris.tracku.Entities.User.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
