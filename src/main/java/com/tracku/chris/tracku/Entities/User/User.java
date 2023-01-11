fa2d06b48709d0ca38eca26ec8cebea5eded81a7
ChrisEs146 <chris.es146@gmail.com>
2023-01-10 23:38:49 -0600

    Update: User entity

---
+++ src/main/java/com/tracku/chris/tracku/Entities/User/User.java
@@ -0,0 +1,73 @@
+package com.tracku.chris.tracku.Entities.User;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name="Users")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long userId;
+    @Column(nullable = false)
+    private String fullName;
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+    @Column(nullable = false, length = 60)
+    private String userPassword;
+    @Column(nullable = false)
+    private LocalDateTime created_at;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 6)
+    private Role role;
+
+    @PrePersist
+    private void onPrePersist() {
+        created_at = LocalDateTime.now();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return userPassword;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
