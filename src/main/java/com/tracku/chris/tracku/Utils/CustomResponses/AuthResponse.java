f1cd1e24f33ea5e8e7545dfc55184bc69a8f2e70
ChrisEs146 <chris.es146@gmail.com>
2023-01-10 23:40:08 -0600

    Add: Auth response DTO

---
+++ src/main/java/com/tracku/chris/tracku/Utils/CustomResponses/AuthResponse.java
@@ -0,0 +1,12 @@
+package com.tracku.chris.tracku.Utils.CustomResponses;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+}
