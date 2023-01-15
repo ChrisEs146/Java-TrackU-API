d2d75eea5c2fbaf35bc6780c14c0a626c6555cd7
ChrisEs146 <chris.es146@gmail.com>
2023-01-14 19:01:17 -0600

    Add: update name request obj

---
+++ src/main/java/com/tracku/chris/tracku/Utils/CustomRequests/Users/UpdateNameRequest.java
@@ -0,0 +1,14 @@
+package com.tracku.chris.tracku.Utils.CustomRequests.Users;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateNameRequest {
+    @NotBlank(message = "Full name can't be empty")
+    @Size(min = 4, message = "Full name should be at least 4 chars long")
+    private String newFullName;
+}
