bd84b75f1fdd536996c37065cc083ae9b181a9c6
ChrisEs146 <chris.es146@gmail.com>
2023-01-14 18:50:10 -0600

    Add: User service interface

--- src/main/java/com/tracku/chris/tracku/Interfaces/User/IUserService.java
+++ src/main/java/com/tracku/chris/tracku/Interfaces/User/IUserService.java
@@ -1,14 +1,12 @@
 package com.tracku.chris.tracku.Interfaces.User;
-import com.tracku.chris.tracku.Utils.CustomRequests.Users.AuthRequest;
-import com.tracku.chris.tracku.Utils.CustomRequests.Users.RegisterRequest;
-import com.tracku.chris.tracku.Utils.CustomRequests.Users.UpdateNameRequest;
-import com.tracku.chris.tracku.Utils.CustomResponses.AuthResponse;
-import com.tracku.chris.tracku.Utils.CustomResponses.RegistrationResponse;
-import com.tracku.chris.tracku.Utils.CustomResponses.UpdateNameResponse;
+import com.tracku.chris.tracku.Utils.CustomRequests.Users.*;
+import com.tracku.chris.tracku.Utils.CustomResponses.*;
 
 public interface IUserService {
     RegistrationResponse registerUser(RegisterRequest request);
     AuthResponse signInUser(AuthRequest request);
     UpdateNameResponse updateUsername(UpdateNameRequest request);
-
+    UpdatePasswordResponse updatePassword(UpdatePasswordRequest request);
+    UserInfoResponse getUserInfo();
+    DeleteUserResponse deleteUser(DeleteUserRequest request);
 }
