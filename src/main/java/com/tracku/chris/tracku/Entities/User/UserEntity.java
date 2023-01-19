d5bbd6ca54e69a2151db89dc3d2e48bad3b57c55
ChrisEs146 <chris.es146@gmail.com>
2023-01-18 23:22:30 -0600

    Update: User entity

--- src/main/java/com/tracku/chris/tracku/Entities/User/UserEntity.java
+++ src/main/java/com/tracku/chris/tracku/Entities/User/UserEntity.java
@@ -1,12 +1,7 @@
 package com.tracku.chris.tracku.Entities.User;
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 import java.time.LocalDateTime;
-import java.util.Collection;
-import java.util.List;
 
 @Entity
 @Getter
@@ -14,60 +9,27 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Table(name="Users")
-public class UserEntity implements UserDetails {
+@Table(name="users")
+public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long userId;
+    private long user_Id;
+
     @Column(nullable = false)
-    private String fullName;
+    private String full_name;
+
     @Column(nullable = false, unique = true, length = 100)
     private String email;
+
     @Column(nullable = false, length = 60)
-    private String userPassword;
+    private String user_password;
+
     @Column(nullable = false)
     private LocalDateTime created_at;
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false, length = 6)
-    private Role role;
 
     @PrePersist
     private void onPrePersist() {
         created_at = LocalDateTime.now();
     }
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(role.name()));
-    }
-
-    @Override
-    public String getPassword() {
-        return userPassword;
-    }
-
-    @Override
-    public String getUsername() {
-        return email;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
 }
