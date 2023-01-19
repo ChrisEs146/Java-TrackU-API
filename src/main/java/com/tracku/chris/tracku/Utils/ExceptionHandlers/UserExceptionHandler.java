086340e19704487d46a4260d7bc9143d6532ae1f
ChrisEs146 <chris.es146@gmail.com>
2023-01-18 23:33:40 -0600

    Update: Exception handlers

--- src/main/java/com/tracku/chris/tracku/Utils/ExceptionHandlers/UserExceptionHandler.java
+++ src/main/java/com/tracku/chris/tracku/Utils/ExceptionHandlers/UserExceptionHandler.java
@@ -6,6 +6,7 @@ import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -23,7 +24,7 @@ public class UserExceptionHandler extends ResponseEntityExceptionHandler {
         return handleExceptionInternal(ex, errorObject, new HttpHeaders(), HttpStatus.CONFLICT, request);
     }
 
-    @ExceptionHandler(value={UserNotFoundException.class, EntityNotFoundException.class})
+    @ExceptionHandler(value={UserNotFoundException.class})
     protected ResponseEntity<Object> handleUserNotFound(EntityNotFoundException ex, WebRequest request) {
         Map<String, Object> errorObject = createErrorObject(HttpStatus.NOT_FOUND, HttpStatus.NOT_FOUND.value(), ex.getMessage());
         return handleExceptionInternal(ex, errorObject, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
@@ -37,10 +38,10 @@ public class UserExceptionHandler extends ResponseEntityExceptionHandler {
 
     private Map<String, Object> createErrorObject(HttpStatus status, int statusCode, String errorMsg) {
         Map<String, Object> errorObject = new HashMap<>();
-        errorObject.put("Timestamp:", LocalDateTime.now());
-        errorObject.put("Status:", status);
-        errorObject.put("StatusCode:", statusCode);
-        errorObject.put("Message:", errorMsg);
+        errorObject.put("Timestamp", LocalDateTime.now());
+        errorObject.put("Status", status);
+        errorObject.put("StatusCode", statusCode);
+        errorObject.put("Message", errorMsg);
         return errorObject;
     }
 }
