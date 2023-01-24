bef37376385602e0041422c94777374d7086eb86
ChrisEs146 <chris.es146@gmail.com>
2023-01-23 23:05:49 -0600

    Update: password response

--- src/main/java/com/tracku/chris/tracku/Utils/CustomResponses/UpdatePasswordResponse.java
+++ src/main/java/com/tracku/chris/tracku/Utils/CustomResponses/UpdatePasswordResponse.java
@@ -1,8 +1,12 @@
 package com.tracku.chris.tracku.Utils.CustomResponses;
 
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Builder
 @NoArgsConstructor
 public class UpdatePasswordResponse {
