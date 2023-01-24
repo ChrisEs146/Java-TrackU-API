c972b8156d39efee84a2fc81f66666411fccfe44
ChrisEs146 <chris.es146@gmail.com>
2023-01-23 23:05:31 -0600

    Update: confirm password

--- src/main/java/com/tracku/chris/tracku/Utils/CustomRequests/Users/UpdatePasswordRequest.java
+++ src/main/java/com/tracku/chris/tracku/Utils/CustomRequests/Users/UpdatePasswordRequest.java
@@ -1,4 +1,5 @@
 package com.tracku.chris.tracku.Utils.CustomRequests.Users;
+import com.tracku.chris.tracku.Utils.CustomValidators.User.ComparePasswords;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.*;
@@ -8,6 +9,7 @@ import lombok.*;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@ComparePasswords(password = "newPassword", confirmPassword = "confirmPassword")
 public class UpdatePasswordRequest {
     @NotBlank(message="Password can't be empty")
     @Pattern(regexp = "^(?=\\S)(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#%^&*])[a-zA-Z0-9!@#%^&*]{8,20}$", message = "Password should be 8-20chars, contain numbers, lowercase and uppercase letters and '!@#%^&*' special chars")
@@ -16,5 +18,5 @@ public class UpdatePasswordRequest {
     @Pattern(regexp = "^(?=\\S)(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#%^&*])[a-zA-Z0-9!@#%^&*]{8,20}$", message = "Password should be 8-20chars, contain numbers, lowercase and uppercase letters and '!@#%^&*' special chars")
     private String newPassword;
     @NotBlank(message="Confirm password can't be empty")
-    private String ConfirmPassword;
+    private String confirmPassword;
 }
